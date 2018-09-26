7f1b0613d015982ac33e13cf29b9d8e8653fbb97
Yadullah Duman <yadullah.duman@gmail.com>
2018-09-26 18:02:38 +0200

    deck update

--- theme.js
+++ theme.js
@@ -1,10 +1,10 @@
-import { future } from "mdx-deck/themes";
-import style from "react-syntax-highlighter/styles/prism/atom-dark";
+import { swiss } from "mdx-deck/themes";
+import style from "react-syntax-highlighter/styles/prism/ghcolors";
 import javascript from "react-syntax-highlighter/languages/prism/javascript";
 
 export default {
-  ...future,
-  font: "Fira Sans, sans-serif",
+  ...swiss,
+  font: "Roboto, sans-serif",
   prism: {
     style,
     languages: {
